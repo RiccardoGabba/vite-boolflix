1daea348abca3cbc6f3808f64c27ec3575bf9626
filippo <riccardo.gabba02@gmail.com>
2023-11-15 15:31:39 +0100

    fixed issues

--- src/App.vue
+++ src/App.vue
@@ -30,13 +30,13 @@ export default {
     getMoviesAndSeries() {
       const movieurl = this.store.apiUrl + this.store.endPoint.movie;
       axios.get(movieurl, { params: this.store.params }).then((rest) => {
-        console.log(res.data.results);
-        this.store.movieList = res.data.results
+        console.log(rest.data.results);
+        this.store.movieList = rest.data.results
       });
       const tvurl = this.store.apiUrl + this.store.endPoint.series;
       axios.get(tvurl, { params: this.store.params }).then((rest) => {
-        console.log(res.data.results);
-        this.store.seriesList = res.data.results
+        console.log(rest.data.results);
+        this.store.seriesList = rest.data.results
       });
     },
   },
